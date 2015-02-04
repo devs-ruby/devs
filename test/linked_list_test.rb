18cd94e06308797405efa65d5717ffc647ba4094
Romain Franceschini <franceschini.romain@gmail.com>
2015-02-04 17:03:32 +0100

    test fix for linked list

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -26,7 +26,7 @@ class TestLinkedList < MiniTest::Test
     @list.shift
     assert_equal 4, @list.size
 
-    @list.delete(4)
+    @list.delete(@list.search(4))
     assert_equal 3, @list.size
 
     @list.pop
@@ -80,8 +80,6 @@ class TestLinkedList < MiniTest::Test
 
     assert_equal nil, @list.search(8)
     assert_equal 1, @list.search(1).value
-    assert_equal [2, 3, 4], @list.to_a
     assert_equal 4, @list.search(4).value
-    assert_equal [2, 3], @list.to_a
   end
 end
