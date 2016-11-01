e3ccd5bf26c64d854a271fd167ecf4197b3c5ebe
Romain Franceschini <romain1189@gmail.com>
2016-11-01 09:46:04 +0100

    new error types

--- lib/devs/errors.rb
+++ lib/devs/errors.rb
@@ -1,8 +1,10 @@
 module DEVS
   class NoSuchChildError < StandardError; end
   class BadSynchronisationError < StandardError; end
+  class NoSuchPortError < Exception; end
   class InvalidPortTypeError < StandardError; end
   class InvalidPortHostError < StandardError; end
   class MessageAlreadySentError < StandardError; end
   class FeedbackLoopError < StandardError; end
+  class UnobservablePortError < StandardError; end
 end
