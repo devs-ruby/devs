c4ea9ba24e6e7c375ffad36ea9014f25ef0cf226
Romain Franceschini <romain1189@gmail.com>
2016-11-02 08:54:32 +0100

    model no longer keep track of its parent

--- lib/devs/model.rb
+++ lib/devs/model.rb
@@ -1,26 +1,20 @@
 module DEVS
   # @abstract Base model class
   class Model
-    attr_accessor :name, :parent, :processor
+    attr_accessor :name, :processor
 
     # @!attribute name
     #   This attribute represent the name of the model.
     #   @return [Symbol] Returns the name of the model.
 
-    # @!attribute parent
+    # @!attribute processor
     #   This attribute represent the associated {Processor}.
     #   @return [Processor] Returns the associated processor.
 
     # Returns a new {Model} instance.
     #
     # @param name [String, Symbol] the name of the model
-    def initialize(name = nil)
-      @name = name.to_sym unless name == nil
-    end
-
-    def name=(name)
-      name = name.to_sym
-      @parent.rename_child(name, @name) if @parent && @name
+    def initialize(name)
       @name = name
     end
 
